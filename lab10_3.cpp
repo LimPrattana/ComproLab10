3f7210a9a1f6b6ea341fb17bf87582f6a43c1410
LimPrattana <vivo.twy0270@gmail.com>
2024-12-17 17:30:24 +0700

    lab10_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ifstream score;
+    score.open("score.txt");
+    string num;
+    int i=0;
+    double sum=0, mean, sd,aa;
+    while ( getline(score, num))
+    {   
+        
+        sum += stod(num);
+        aa += pow(stod(num),2);
+        i++;
+    }
+    mean = sum/i;
+    sd = sqrt((aa/i)-pow(mean,2));
+
+    cout << "Number of data = "<<i<<endl;
+    cout << setprecision(3);
+    cout << "Mean = "<<mean<<endl;
+    cout << "Standard deviation = "<<sd<<endl;
+}
